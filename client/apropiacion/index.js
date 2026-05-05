72fc07f14576499fc0735a1957ee5e57848036ff
sebastian <falconsebas23@gmail.com>
2026-05-05 08:37:14 -0500

    (apropiacion)realizar ejercicio5 (#5)

--- client/apropiacion/index.js
+++ client/apropiacion/index.js
@@ -2,5 +2,6 @@ export { usuarios, usuario, posts } from "./ejercicio1.js";
 export { post, comment } from "./ejercicio2.js";
 export { actualizarPostPUT, actualizarPostPATCH } from "./ejercicio3.js";
 export { eliminarPublicacion } from "./ejercicio4.js";
+export { obtenerPublicacion, obtenerPublicaciones } from "./ejercicio5.js";
 
 
