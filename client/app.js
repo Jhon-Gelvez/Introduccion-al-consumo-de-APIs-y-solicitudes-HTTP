3cb3bfbac27d09a9848b9fdfe1ebcef3df4f9ae2
sebastian <falconsebas23@gmail.com>
2026-05-05 08:19:21 -0500

    (apropiacion) relizar los import

--- client/app.js
+++ client/app.js
@@ -1,6 +1,5 @@
-import { usuario, usuarios, posts, post, comment, actualizarPostPATCH, actualizarPostPUT } from "./apropiacion/index.js";
+import { usuario, usuarios, posts, post, comment, actualizarPostPATCH, actualizarPostPUT, eliminarPublicacion } from "./apropiacion/index.js";
 import { enunciado1, publicaciones } from "./transferencia/index.js";
-import { eliminarPublicacion } from "./apropiacion/index.js";
 import readline from "node:readline/promises";
 import { stdin as input, stdout as output } from "node:process";
 
